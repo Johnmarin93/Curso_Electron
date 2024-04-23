2884062c59b1a5dafae4ae40e75fd706179b61f8
Johnmarin93 <Johnmarin93@users.noreply.github.com>
2024-04-23 18:30:59 -0500

    second commit

---
+++ App01-Marcadores/main.js
@@ -0,0 +1,27 @@
+const {app, BrowserWindow} = require('electron');
+
+function crearVentanaPrincipal() {
+    let ventanaPrincipal = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    ventanaPrincipal.loadFile('index.html');
+}
+
+app.whenReady().then(crearVentanaPrincipal);
+
+app.on('window-all-closed', function(){
+    if (process.platform !== 'darwin'){
+        app.quit();
+    }
+});
+
+app.on('activate', function() {
+    if(BrowserWindow.getAllWindows().length === 0){
+        crearVentanaPrincipal();
+    }
+});
